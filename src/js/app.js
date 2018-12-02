5bae269a411f66afc0a5d9c8324a2d74e1a74d40
eladr <eladr@post.bgu.ac.il>
2018-12-02 14:13:36 +0200

    started working. Need to figure out how to use the eval function with variable names

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
-import {substitution} from './code-analyzer';
+import {substitute} from './code-analyzer';
+import * as escodegen from 'escodegen';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         let substitution = substitute(parsedCode)
-        $('#parsedCode').val(JSON.stringify(substitution, null, 2));
+        $('#parsedCode').val(escodegen.generate(substitution));
     });
 });
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,29 +1,62 @@
 import * as esprima from 'esprima';
+import * as escodegen from 'escodegen';
+
+var values = {}
+var global_i = 0
+
+function jsonEqual(a,b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+}
 
 const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse);
 };
 
-function parseBody(ast, rows){
+function parseBody(ast){
     if(ast.body.constructor === Array){
-        for(var row in ast.body)
-            substitute(ast.body[row], ast);
+        for (global_i = 0; global_i < ast.body.length; global_i++)
+            substitute(ast.body[global_i], ast.body)
         if(ast.type == 'Program')
-            return rows;
+            return ast;
     }
     else
         substitute(ast.body, ast);
 }
 
 function parseExpressionStatement(ast, father){
-    father.remove(ast);
+    for(var row in father)
+        if(jsonEqual(father[row], ast))
+            father.splice(row, 1);
+}
+
+function parseVariableDeclaration(ast, father){
+    for(let decl in ast.declarations){
+        Object.keys(values).forEach(function(key) {
+            let val = escodegen.generate(ast.declarations[decl].init).replace(key, values[key]);
+            values[ast.declarations[decl].id.name] = val;
+        });
+    }
+    for(var row in father)
+        if(jsonEqual(father[row], ast)){
+            father.splice(row, 1);
+            global_i--;
+        }
+
+}
+
+function parseFunctionDeclaration(ast, father){
+    for(var param in ast.params)
+        values[ast.params[param].name] = ast.params[param].name;
 }
 
 var parseFunctions = {
     'ExpressionStatement': parseExpressionStatement,
+    'VariableDeclaration': parseVariableDeclaration,
+    'FunctionDeclaration': parseFunctionDeclaration
 };
 
 function substitute (ast, father=null){
+    console.log(values);
     if(ast == null)
         return;
     if(parseFunctions.hasOwnProperty(ast.type))
