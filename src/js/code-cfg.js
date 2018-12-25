0a92af5102f02769b8fec0ea4bc44b573ac7b5f0
eladr <eladr@post.bgu.ac.il>
2018-12-25 16:20:31 +0200

    some fixes

--- src/js/code-cfg.js
+++ src/js/code-cfg.js
@@ -2,8 +2,11 @@ import * as escodegen from 'escodegen';
 import * as esgraph from 'esgraph';
 
 function makeGraph(code){
-    let funcArr = code.body.filter(exp => exp.type == 'FunctionDeclaration');
-    let func = funcArr[0].body;
+    let funcArr = undefined;
+    for(let n in code.body)
+        if(code.body[n].type == 'FunctionDeclaration')
+            funcArr = code.body[n];
+    let func = funcArr.body;
     let graph = esgraph(func)[2];
     for(let n in graph)
         delete graph[n].exception;
@@ -20,16 +23,18 @@ function cleanEntryExit(graph){
     realEntry.type = 'entry';
     realEntry.prev = [];
     graph.splice(0, 1);
-    let exitNode = graph[graph.length - 1];
-    let realExit = exitNode.prev.filter(node => node.astNode.type == 'ReturnStatement');
-    realExit = realExit[0];
+    let exit = graph[graph.length - 1];
+    let realExit = undefined;
+    for(let n in exit.prev)
+        if(exit.prev[n].astNode.type == 'ReturnStatement')
+            realExit = exit.prev[n];
     realExit.next = [];
     realExit.type = 'exit';
     delete realExit.normal;
     graph.splice(graph.length - 1, 1);
     for(let n in graph)
-        if(graph[n].next.indexOf(exitNode) != -1)
-            graph[n].next.splice(graph[n].next.indexOf(exitNode), 1);
+        if(graph[n].next.indexOf(exit) != -1)
+            graph[n].next.splice(graph[n].next.indexOf(exit), 1);
 }
 
 function fixNormal(graph){
